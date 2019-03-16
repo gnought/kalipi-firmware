2632f5bd77cb005d104113d7fd9b386d09dd1a29
Re4son <Re4son@users.noreply.github.com>
2019-03-16 21:09:39 +1100

    Add missing firmware

--- opt/vc/include/interface/vcos/vcos_build_info.h
+++ opt/vc/include/interface/vcos/vcos_build_info.h
@@ -29,5 +29,4 @@ const char *vcos_get_build_hostname( void );
 const char *vcos_get_build_version( void );
 const char *vcos_get_build_time( void );
 const char *vcos_get_build_date( void );
-const char *vcos_get_build_variant( void );
 
