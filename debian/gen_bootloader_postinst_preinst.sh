61ee17304febab2618ee1394233a100fb0fd63a5
Re4son <Re4son@users.noreply.github.com>
2019-06-29 19:15:59 +1000

    Add support for bcm2711
    Signed-off-by: Re4son <Re4son@users.noreply.github.com>

--- debian/gen_bootloader_postinst_preinst.sh
+++ debian/gen_bootloader_postinst_preinst.sh
@@ -12,7 +12,7 @@ version=`cat ../extra/uname_string | cut -f 3 -d ' ' | tr -d +`-Re4son
 
 ###   kalipi-kernel.postinst
 printf "#!/bin/sh\n" > kalipi-kernel.postinst
-printf 'for file in kernel.img \\\n            kernel7.img \\\n            kernel8-alt.img \\\n' >> kalipi-kernel.postinst
+printf 'for file in kernel.img \\\n            kernel7.img \\\n            kernel7l.img \\\n            kernel8-alt.img \\\n            kernel8l-alt.img \\\n' >> kalipi-kernel.postinst
 
 ###   kalipi-kernel.preinst
 cat <<EOF > kalipi-kernel.preinst
@@ -32,7 +32,9 @@ VER=\$(dpkg-query -f='\${Version}' --show dpkg)
 MINVER=1.19.1
 for file in kernel.img \\
             kernel7.img \\
+            kernel7l.img \\
             kernel8-alt.img \\
+            kernel8l-alt.img \\
 EOF
 
 
@@ -69,7 +71,9 @@ fi
 if [ -d "/etc/kernel/preinst.d" ]; then
   run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/preinst.d
   run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/preinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/preinst.d
   run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/preinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/preinst.d
 fi
 if [ -d "/etc/kernel/preinst.d/${version}+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/preinst.d/${version}+
@@ -77,8 +81,14 @@ fi
 if [ -d "/etc/kernel/preinst.d/${version}-v7+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/preinst.d/${version}-v7+
 fi
+if [ -d "/etc/kernel/preinst.d/${version}-v7l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/preinst.d/${version}-v7l+
+fi
 if [ -d "/etc/kernel/preinst.d/${version}-v8+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/preinst.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/preinst.d/${version}-v8l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/preinst.d/${version}-v8l+
 fi
 EOF
 
@@ -87,7 +97,7 @@ cat <<EOF >> kalipi-kernel.postinst
             ; do
     if [ -f /usr/share/rpikernelhack/\$file ]; then
         rm -f /boot/\$file
-        dpkg-divert --package rpikernelhack --remove --rename /boot/\$file
+        dpkg-divert --package rpikernelhack --rename --remove /boot/\$file
         sync
     fi
 done
@@ -101,7 +111,9 @@ fi
 if [ -d "/etc/kernel/postinst.d" ]; then
   run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/postinst.d
   run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/postinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/postinst.d
   run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/postinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/postinst.d
 fi
 if [ -d "/etc/kernel/postinst.d/${version}+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/postinst.d/${version}+
@@ -109,8 +121,14 @@ fi
 if [ -d "/etc/kernel/postinst.d/${version}-v7+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/postinst.d/${version}-v7+
 fi
+if [ -d "/etc/kernel/postinst.d/${version}-v7l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/postinst.d/${version}-v7l+
+fi
 if [ -d "/etc/kernel/postinst.d/${version}-v8+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/postinst.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/postinst.d/${version}-v8l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/postinst.d/${version}-v8l+
 fi
 EOF
 
@@ -118,9 +136,9 @@ EOF
 printf "#DEBHELPER#\n" >> kalipi-kernel.postinst
 printf "#DEBHELPER#\n" >> kalipi-kernel.preinst
 
-printf "#!/bin/sh\n" > kalipi-bootloader.postinst
+printf "#!/bin/sh -e\n" > kalipi-bootloader.postinst
 printf 'for file in \\\n' >> kalipi-bootloader.postinst
-printf "#!/bin/sh\n" > kalipi-bootloader.preinst
+printf "#!/bin/sh -e\n" > kalipi-bootloader.preinst
 
 printf "mkdir -p /usr/share/rpikernelhack\n" >> kalipi-bootloader.preinst
 
@@ -172,7 +190,7 @@ cat <<EOF >> kalipi-bootloader.preinst
     if \$(dpkg --compare-versions \$VER ge \$MINVER); then
         dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/\$file --no-rename /boot/\$file
     else
-	dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/\$file /boot/\$file
+	dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/\$file --rename /boot/\$file
     fi
 done
 #DEBHELPER#
@@ -180,7 +198,7 @@ EOF
 
 cat <<EOF >> kalipi-bootloader.postinst
             ; do
-    rm -f \$file
+    rm -f /boot/\$file
     dpkg-divert --package rpikernelhack --remove --rename /boot/\$file
     sync
 done
@@ -205,7 +223,9 @@ fi
 if [ -d "/etc/kernel/prerm.d" ]; then
   run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/prerm.d
   run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/prerm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/prerm.d
   run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/prerm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/prerm.d
 fi
 if [ -d "/etc/kernel/prerm.d/${version}+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/prerm.d/${version}+
@@ -213,8 +233,14 @@ fi
 if [ -d "/etc/kernel/prerm.d/${version}-v7+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/prerm.d/${version}-v7+
 fi
+if [ -d "/etc/kernel/prerm.d/${version}-v7l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/prerm.d/${version}-v7l+
+fi
 if [ -d "/etc/kernel/prerm.d/${version}-v8+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/prerm.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/prerm.d/${version}-v8l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/prerm.d/${version}-v8l+
 fi
 EOF
 
@@ -229,7 +255,9 @@ fi
 if [ -d "/etc/kernel/postrm.d" ]; then
   run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/postrm.d
   run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/postrm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/postrm.d
   run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/postrm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/postrm.d
 fi
 if [ -d "/etc/kernel/postrm.d/${version}+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/postrm.d/${version}+
@@ -237,8 +265,14 @@ fi
 if [ -d "/etc/kernel/postrm.d/${version}-v7+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/postrm.d/${version}-v7+
 fi
+if [ -d "/etc/kernel/postrm.d/${version}-v7l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/postrm.d/${version}-v7l+
+fi
 if [ -d "/etc/kernel/postrm.d/${version}-v8+" ]; then
   run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/postrm.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/postrm.d/${version}-v8l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/postrm.d/${version}-v8l+
 fi
 EOF
 
@@ -253,7 +287,9 @@ fi
 if [ -d "/etc/kernel/header_postinst.d" ]; then
   run-parts -v --verbose --exit-on-error --arg=${version}+ /etc/kernel/header_postinst.d
   run-parts -v --verbose --exit-on-error --arg=${version}-v7+ /etc/kernel/header_postinst.d
+  run-parts -v --verbose --exit-on-error --arg=${version}-v7l+ /etc/kernel/header_postinst.d
   run-parts -v --verbose --exit-on-error --arg=${version}-v8+ /etc/kernel/header_postinst.d
+  run-parts -v --verbose --exit-on-error --arg=${version}-v8l+ /etc/kernel/header_postinst.d
 fi
 if [ -d "/etc/kernel/header_postinst.d/${version}+" ]; then
   run-parts -v --verbose --exit-on-error --arg=${version}+ /etc/kernel/header_postinst.d/${version}+
@@ -261,8 +297,14 @@ fi
 if [ -d "/etc/kernel/header_postinst.d/${version}-v7+" ]; then
   run-parts -v --verbose --exit-on-error --arg=${version}-v7+ /etc/kernel/header_postinst.d/${version}-v7+
 fi
+if [ -d "/etc/kernel/header_postinst.d/${version}-v7l+" ]; then
+  run-parts -v --verbose --exit-on-error --arg=${version}-v7l+ /etc/kernel/header_postinst.d/${version}-v7l+
+fi
 if [ -d "/etc/kernel/header_postinst.d/${version}-v8+" ]; then
   run-parts -v --verbose --exit-on-error --arg=${version}-v8+ /etc/kernel/header_postinst.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/header_postinst.d/${version}-v8l+" ]; then
+  run-parts -v --verbose --exit-on-error --arg=${version}-v8l+ /etc/kernel/header_postinst.d/${version}-v8l+
 fi
 EOF
 
