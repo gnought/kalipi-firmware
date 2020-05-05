2900d78e06bf3cc23c22904047f96e519e546bbf
Re4son <Re4son@users.noreply.github.com>
2020-05-05 11:56:32 +1000

    20200212kali release

---
+++ debian/gen_bootloader_postinst_preinst.sh
@@ -0,0 +1,314 @@
+#!/bin/sh
+
+if ! [ -d ../boot ]; then
+  printf "Can't find boot dir. Run from debian subdir\n"
+  exit 1
+fi
+
+version=`cat ../extra/uname_string | cut -f 3 -d ' ' | tr -d +`-Re4son
+
+#     create kalipi-kernel preinst/postinst scripts
+##    headers
+
+###   kalipi-kernel.postinst
+printf "#!/bin/sh\n" > kalipi-kernel.postinst
+printf 'for file in kernel.img \\\n            kernel7.img \\\n            kernel7l.img \\\n            kernel8-alt.img \\\n            kernel8l-alt.img \\\n' >> kalipi-kernel.postinst
+
+###   kalipi-kernel.preinst
+cat <<EOF > kalipi-kernel.preinst
+#!/bin/sh
+
+if ! grep -q boot /proc/mounts; then
+    mount /boot
+fi
+
+mkdir -p /usr/share/rpikernelhack/overlays
+mkdir -p /boot/overlays
+# https://git.dpkg.org/cgit/dpkg/dpkg.git/commit/?id=599e3c1a9f3be8687c00b681f107e7b98bb454ae
+# We have to add "--no-rename" explicitly for dpkg versions >= 1.19.1
+# But this will fail for versions < 1.19.1 so we detect the version to choose the right command line below
+# to be compatible with older versions as well as Raspbian and other distros
+VER=\$(dpkg-query -f='\${Version}' --show dpkg)
+MINVER=1.19.1
+for file in kernel.img \\
+            kernel7.img \\
+            kernel7l.img \\
+            kernel8-alt.img \\
+            kernel8l-alt.img \\
+EOF
+
+
+##    content
+
+for FN in ../boot/*.dtb ../boot/COPYING.linux ../boot/overlays/*; do
+  if ! [ -d "\$FN" ]; then
+    FN=${FN#../boot/}
+    printf "            $FN" >> kalipi-kernel.preinst
+    printf ' \\\n' >> kalipi-kernel.preinst
+    printf "            $FN" >> kalipi-kernel.postinst
+    printf ' \\\n' >> kalipi-kernel.postinst
+  fi
+done
+
+
+##    footer
+###   kalipi-kernel.preinst
+cat <<EOF >> kalipi-kernel.preinst
+            ;do
+    if \$(dpkg --compare-versions \$VER ge \$MINVER); then
+        dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/\$file --no-rename /boot/\$file
+    else
+	dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/\$file /boot/\$file
+    fi
+done
+if [ -f /etc/default/kalipi-kernel ]; then
+  . /etc/default/kalipi-kernel
+  INITRD=\${INITRD:-"No"}
+  export INITRD
+  RPI_INITRD=\${RPI_INITRD:-"No"}
+  export RPI_INITRD
+fi
+if [ -d "/etc/kernel/preinst.d" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/preinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/preinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/preinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/preinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/preinst.d
+fi
+if [ -d "/etc/kernel/preinst.d/${version}+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/preinst.d/${version}+
+fi
+if [ -d "/etc/kernel/preinst.d/${version}-v7+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/preinst.d/${version}-v7+
+fi
+if [ -d "/etc/kernel/preinst.d/${version}-v7l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/preinst.d/${version}-v7l+
+fi
+if [ -d "/etc/kernel/preinst.d/${version}-v8+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/preinst.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/preinst.d/${version}-v8l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/preinst.d/${version}-v8l+
+fi
+EOF
+
+###   kalipi-kernel.postinst
+cat <<EOF >> kalipi-kernel.postinst
+            ; do
+    if [ -f /usr/share/rpikernelhack/\$file ]; then
+        rm -f /boot/\$file
+        dpkg-divert --package rpikernelhack --rename --remove /boot/\$file
+        sync
+    fi
+done
+if [ -f /etc/default/kalipi-kernel ]; then
+  . /etc/default/kalipi-kernel
+  INITRD=\${INITRD:-"No"}
+  export INITRD
+  RPI_INITRD=\${RPI_INITRD:-"No"}
+  export RPI_INITRD
+fi
+if [ -d "/etc/kernel/postinst.d" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/postinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/postinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/postinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/postinst.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/postinst.d
+fi
+if [ -d "/etc/kernel/postinst.d/${version}+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/postinst.d/${version}+
+fi
+if [ -d "/etc/kernel/postinst.d/${version}-v7+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/postinst.d/${version}-v7+
+fi
+if [ -d "/etc/kernel/postinst.d/${version}-v7l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/postinst.d/${version}-v7l+
+fi
+if [ -d "/etc/kernel/postinst.d/${version}-v8+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/postinst.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/postinst.d/${version}-v8l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/postinst.d/${version}-v8l+
+fi
+EOF
+
+#     Create all other installation scripts
+printf "#DEBHELPER#\n" >> kalipi-kernel.postinst
+printf "#DEBHELPER#\n" >> kalipi-kernel.preinst
+
+printf "#!/bin/sh -e\n" > kalipi-bootloader.postinst
+printf 'for file in \\\n' >> kalipi-bootloader.postinst
+printf "#!/bin/sh -e\n" > kalipi-bootloader.preinst
+
+printf "mkdir -p /usr/share/rpikernelhack\n" >> kalipi-bootloader.preinst
+
+cat <<EOF >> kalipi-bootloader.preinst
+if [ -f "/boot/recovery.elf" ]; then
+  echo "/boot appears to be NOOBS recovery partition. Applying fix."
+  rootnum=\`cat /proc/cmdline | sed -n 's|.*root=/dev/mmcblk0p\([0-9]*\).*|\1|p'\`
+  if [ ! "\$rootnum" ];then
+    echo "Could not determine root partition"
+    exit 1
+  fi
+  if ! grep -qE "/dev/mmcblk0p1\s+/boot" /etc/fstab; then
+    echo "Unexpected fstab entry"
+    exit 1
+  fi
+  boot="/dev/mmcblk0p\$((rootnum-1))"
+  root="/dev/mmcblk0p\${rootnum}"
+  sed /etc/fstab -i -e "s|^.* / |\${root}  / |"
+  sed /etc/fstab -i -e "s|^.* /boot |\${boot}  /boot |"
+  umount /boot
+  if [ \$? -ne 0 ]; then
+    echo "Failed to umount /boot. Remount manually and run sudo apt-get install -f."
+    exit 1
+  else
+    mount /boot
+  fi
+fi
+# https://git.dpkg.org/cgit/dpkg/dpkg.git/commit/?id=599e3c1a9f3be8687c00b681f107e7b98bb454ae
+# We have to add "--no-rename" explicitly for dpkg versions >= 1.19.1
+# But this will fail for versions < 1.19.1 so we detect the version to choose the right command line below
+# to be compatible with older versions as well as Raspbian and other distros
+VER=\$(dpkg-query -f='\${Version}' --show dpkg)
+MINVER=1.19.1
+for file in \\
+EOF
+
+for FN in ../boot/*.elf ../boot/*.dat ../boot/*.bin ../boot/LICENCE.broadcom; do
+  if ! [ -d "\$FN" ]; then
+    FN=${FN#../boot/}
+    printf "            $FN" >> kalipi-bootloader.preinst
+    printf ' \\\n' >> kalipi-bootloader.preinst
+    printf "            $FN" >> kalipi-bootloader.postinst
+    printf ' \\\n' >> kalipi-bootloader.postinst
+  fi
+done
+
+cat <<EOF >> kalipi-bootloader.preinst
+            ;do
+    if \$(dpkg --compare-versions \$VER ge \$MINVER); then
+        dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/\$file --no-rename /boot/\$file
+    else
+	dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/\$file --rename /boot/\$file
+    fi
+done
+#DEBHELPER#
+EOF
+
+cat <<EOF >> kalipi-bootloader.postinst
+            ; do
+    rm -f /boot/\$file
+    dpkg-divert --package rpikernelhack --remove --rename /boot/\$file
+    sync
+done
+#DEBHELPER#
+EOF
+
+
+
+
+printf "#!/bin/sh\n" > kalipi-kernel.prerm
+printf "#!/bin/sh\n" > kalipi-kernel.postrm
+printf "#!/bin/sh\n" > kalipi-kernel-headers.postinst
+
+cat <<EOF >> kalipi-kernel.prerm
+if [ -f /etc/default/kalipi-kernel ]; then
+  . /etc/default/kalipi-kernel
+  INITRD=\${INITRD:-"No"}
+  export INITRD
+  RPI_INITRD=\${RPI_INITRD:-"No"}
+  export RPI_INITRD
+fi
+if [ -d "/etc/kernel/prerm.d" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/prerm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/prerm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/prerm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/prerm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/prerm.d
+fi
+if [ -d "/etc/kernel/prerm.d/${version}+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/prerm.d/${version}+
+fi
+if [ -d "/etc/kernel/prerm.d/${version}-v7+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/prerm.d/${version}-v7+
+fi
+if [ -d "/etc/kernel/prerm.d/${version}-v7l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/prerm.d/${version}-v7l+
+fi
+if [ -d "/etc/kernel/prerm.d/${version}-v8+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/prerm.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/prerm.d/${version}-v8l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/prerm.d/${version}-v8l+
+fi
+EOF
+
+cat <<EOF >> kalipi-kernel.postrm
+if [ -f /etc/default/kalipi-kernel ]; then
+  . /etc/default/kalipi-kernel
+  INITRD=\${INITRD:-"No"}
+  export INITRD
+  RPI_INITRD=\${RPI_INITRD:-"No"}
+  export RPI_INITRD
+fi
+if [ -d "/etc/kernel/postrm.d" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/postrm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/postrm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/postrm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/postrm.d
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/postrm.d
+fi
+if [ -d "/etc/kernel/postrm.d/${version}+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}+ --arg=/boot/kernel.img /etc/kernel/postrm.d/${version}+
+fi
+if [ -d "/etc/kernel/postrm.d/${version}-v7+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7+ --arg=/boot/kernel7.img /etc/kernel/postrm.d/${version}-v7+
+fi
+if [ -d "/etc/kernel/postrm.d/${version}-v7l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v7l+ --arg=/boot/kernel7l.img /etc/kernel/postrm.d/${version}-v7l+
+fi
+if [ -d "/etc/kernel/postrm.d/${version}-v8+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8+ --arg=/boot/kernel8-alt.img /etc/kernel/postrm.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/postrm.d/${version}-v8l+" ]; then
+  run-parts -v --report --exit-on-error --arg=${version}-v8l+ --arg=/boot/kernel8l-alt.img /etc/kernel/postrm.d/${version}-v8l+
+fi
+EOF
+
+cat <<EOF >> kalipi-kernel-headers.postinst
+if [ -f /etc/default/kalipi-kernel ]; then
+  . /etc/default/kalipi-kernel
+  INITRD=\${INITRD:-"No"}
+  export INITRD
+  RPI_INITRD=\${RPI_INITRD:-"No"}
+  export RPI_INITRD
+fi
+if [ -d "/etc/kernel/header_postinst.d" ]; then
+  run-parts -v --verbose --exit-on-error --arg=${version}+ /etc/kernel/header_postinst.d
+  run-parts -v --verbose --exit-on-error --arg=${version}-v7+ /etc/kernel/header_postinst.d
+  run-parts -v --verbose --exit-on-error --arg=${version}-v7l+ /etc/kernel/header_postinst.d
+  run-parts -v --verbose --exit-on-error --arg=${version}-v8+ /etc/kernel/header_postinst.d
+  run-parts -v --verbose --exit-on-error --arg=${version}-v8l+ /etc/kernel/header_postinst.d
+fi
+if [ -d "/etc/kernel/header_postinst.d/${version}+" ]; then
+  run-parts -v --verbose --exit-on-error --arg=${version}+ /etc/kernel/header_postinst.d/${version}+
+fi
+if [ -d "/etc/kernel/header_postinst.d/${version}-v7+" ]; then
+  run-parts -v --verbose --exit-on-error --arg=${version}-v7+ /etc/kernel/header_postinst.d/${version}-v7+
+fi
+if [ -d "/etc/kernel/header_postinst.d/${version}-v7l+" ]; then
+  run-parts -v --verbose --exit-on-error --arg=${version}-v7l+ /etc/kernel/header_postinst.d/${version}-v7l+
+fi
+if [ -d "/etc/kernel/header_postinst.d/${version}-v8+" ]; then
+  run-parts -v --verbose --exit-on-error --arg=${version}-v8+ /etc/kernel/header_postinst.d/${version}-v8+
+fi
+if [ -d "/etc/kernel/header_postinst.d/${version}-v8l+" ]; then
+  run-parts -v --verbose --exit-on-error --arg=${version}-v8l+ /etc/kernel/header_postinst.d/${version}-v8l+
+fi
+EOF
+
+printf "#DEBHELPER#\n" >> kalipi-kernel.prerm
+printf "#DEBHELPER#\n" >> kalipi-kernel.postrm
+printf "#DEBHELPER#\n" >> kalipi-kernel-headers.postinst
+
---
+++ debian/update.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -e
+if ! [ -f ./debian/update.sh ]; then
+  printf "Cannot find working directory. Run from from root directory:\n./debian/update.sh\n"
+  exit 1
+fi
+
+old_version=`cat extra/uname_string | cut -f 3 -d ' ' | tr -d +`
+##UPSTREAM_BRANCH="master"
+UPSTREAM_BRANCH="stable"
+
+git fetch --all
+if [ -n "$1" ]; then
+	FIRMWARE_COMMIT="$1"
+else
+	FIRMWARE_COMMIT="`git rev-parse upstream/$UPSTREAM_BRANCH`"
+fi
+
+git checkout $UPSTREAM_BRANCH
+git merge $FIRMWARE_COMMIT --no-edit
+piversion=`cat extra/uname_string | cut -f 3 -d ' ' | tr -d +`
+version=`cat extra/uname_string | cut -f 3 -d ' ' | tr -d +`-Re4son
+version6="$version+"
+version7="$version-v7+"
+version7l="$version-v7l+"
+version8="$version-v8+"
+version8l="$version-v8l+"
+git checkout --orphan $piversion 
+git add -A
+git commit -am "Create $piversion Firmware"
+
+DATE="`git show -s --format=%ct $FIRMWARE_COMMIT`"
+DEBVER="`date -d @$DATE -u +%Y%m%dkali-1`"
+RELEASE="`date -d @$DATE -u +%Y%m%dkali`"
+
+KERNEL_COMMIT="`cat extra/git_hash`"
+
+
+echo Cleaning up Raspberrypi firmware ...
+rm -rf headers modules
+rm -f extra/*symvers extra/*.map boot/*.img
+
+echo Pulling back Kali customisations
+git checkout $old_version debian
+git checkout $old_version lib/firmware/brcm
+git checkout $old_version boot/overlays/35-hat.dtbo
+git checkout $old_version boot/overlays/4dpi-32.dtbo
+git checkout $old_version boot/overlays/elecfreaks22-overlay.dtb
+git checkout $old_version boot/overlays/elecfreaks22.dtbo
+git checkout $old_version boot/overlays/hyperpixel-gpio-backlight.dtbo
+git checkout $old_version boot/overlays/hyperpixel.dtbo
+git checkout $old_version boot/overlays/hyperpixel4-touch.dtbo
+git checkout $old_version boot/overlays/hyperpixel4.dtbo
+git checkout $old_version boot/overlays/jbtek-overlay.dtb
+git checkout $old_version boot/overlays/jbtek.dtbo
+git checkout $old_version boot/overlays/sainsmart32-overlay.dtb
+git checkout $old_version boot/overlays/sainsmart32.dtbo
+git checkout $old_version boot/overlays/waveshare32b-overlay.dtb
+git checkout $old_version boot/overlays/waveshare32b.dtbo
+git checkout $old_version boot/overlays/waveshare35a-overlay.dtb
+git checkout $old_version boot/overlays/waveshare35a.dtbo
+git checkout $old_version boot/overlays/waveshare35b-overlay.dtb
+git checkout $old_version boot/overlays/waveshare35b.dtbo
+
+
+(cd debian; ./gen_bootloader_postinst_preinst.sh)
+dch -v $DEBVER -D kali-rolling --force-distribution "firmware as of ${FIRMWARE_COMMIT}"
+git commit -a -m "$RELEASE release"
+git tag $RELEASE $FIRMWARE_COMMIT
